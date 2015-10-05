b84f6f0999ef21212363cf1406e5b4173fd82843
Yiming Chen <dsdshcym@gmail.com>
2015-10-05 15:39:28 +0800

    Add test for selfReplacement

--- DES/des_test.py
+++ DES/des_test.py
@@ -8,5 +8,10 @@ class TestDESAlgorithm(unittest.TestCase):
         self.assertEqual(des.leftShift(bits, 1), [1, 0, 0, 1, 0, 1, 1])
         self.assertEqual(des.leftShift(bits, 0), [1, 1, 0, 0, 1, 0, 1])
 
+    def test_selfReplacement(self):
+        bits = [1, 0, 1, 1, 1, 0, 1, 0]
+        table = [2, 4, 5, 7, 1, 3, 0, 6]
+        self.assertEqual(des.selfReplacement(bits, table), [1, 1, 0, 0, 0, 1, 1, 1])
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDESAlgorithm)
 unittest.TextTestRunner(verbosity=2).run(suite)
