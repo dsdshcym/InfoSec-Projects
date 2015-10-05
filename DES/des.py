779f7c3ce93de0b868dd40f4f4402f27b598963c
Yiming Chen <dsdshcym@gmail.com>
2015-10-05 15:40:45 +0800

    Add selfReplacement

--- DES/des.py
+++ DES/des.py
@@ -2,3 +2,9 @@
 
 def leftShift(bits, step):
     return bits[step:] + bits[:step]
+
+def selfReplacement(bits, replace_table):
+    new_bits = list(bits)
+    for i, pos in enumerate(replace_table):
+        new_bits[i] = bits[pos]
+    return new_bits
